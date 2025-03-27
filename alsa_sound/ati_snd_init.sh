e0e00e9b52fbfd31c760946d097ac3113900e82e
atipiyush <piyush.thange@atimotors.com>
2025-03-27 11:41:53 +0530

    Refactoring and applying check (#6)
    * Waiting for bootup and then resetting the sound params
    
    Signed-off-by: Piyush Thange <piyush.thange@atimotors.com>
    
    * removed ati_snd_check as it's not required. Implemented the sound check in ati_snd_init.sh
    
    Signed-off-by: Piyush Thange <piyush.thange@atimotors.com>
    
    * removed sndcheck.service
    
    Signed-off-by: Piyush Thange <piyush.thange@atimotors.com>
    
    ---------
    
    Signed-off-by: Piyush Thange <piyush.thange@atimotors.com>

--- alsa_sound/ati_snd_init.sh
+++ alsa_sound/ati_snd_init.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-amixer -c APE cset name="I2S5 Mux" ADMAIF5
-amixer -c APE cset name="H40-SGTL Lineout Playback Switch" "on"
-amixer -c APE cset name="H40-SGTL Lineout Playback Volume" 40
-alsactl restore
+wait_set() {
+    # Wait until the system is fully running
+    systemd-analyze > /dev/null
+    while [ $? -ne 0 ]; do
+        sleep 60
+        systemd-analyze > /dev/null
+    done
+
+	amixer -c APE cset name="I2S5 Mux" ADMAIF5
+	amixer -c APE cset name="H40-SGTL Lineout Playback Switch" "on"
+	amixer -c APE cset name="H40-SGTL Lineout Playback Volume" 40
+	alsactl restore
+
+	/bin/aplay /home/ati/beep.wav
+}
+
+wait_set
